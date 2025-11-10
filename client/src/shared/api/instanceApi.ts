aced85f7283668c5f9e2ee54b7595c02d114ccc8
executed <execute.eee@yandex.ru>
2025-11-10 21:32:45 +0300

    add project structure

---
+++ client/src/shared/api/instanceApi.ts
@@ -0,0 +1,33 @@
+const BASE_URL = 'http://localhost:3000';
+
+class ApiError extends Error {
+  constructor(response: Response) {
+    super(`ApiError: ${response.status} ${response.statusText}`);
+  }
+}
+
+export const jsonApiInstance = async <T>(
+  url: string,
+  init?: RequestInit & { json?: unknown },
+): Promise<T> => {
+  let headers = init?.headers ?? {};
+  if (init?.json) {
+    headers = {
+      ...headers,
+      'Content-Type': 'application/json',
+    };
+    init.body = JSON.stringify(init.json);
+  }
+
+  const result = await fetch(`${BASE_URL}${url}`, {
+    ...init,
+    headers,
+  });
+
+  if (!result.ok) {
+    throw new ApiError(result);
+  }
+
+  const data = (await result.json()) as T;
+  return data;
+};
